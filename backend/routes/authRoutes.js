02a4a46e44e0b5c27fa10025d83392edca0289ca
balanagasri <balanagasrithota2005@gmail.com>
2025-06-29 03:06:55 +0530

    authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,41 +1,8 @@
 const express = require('express');
-const passport = require('passport');
 const router = express.Router();
+const { registerVolunteer, loginUser } = require('../controllers/authController');
 
-const {
-  registerUser,
-  loginUser,
-  logoutUser,
-  verifyEmail,
-  forgotPassword,
-  resetPassword,
-  getCurrentUser
-} = require('../controllers/authController');
-
-// Register
-router.post('/register', registerUser);
-
-// Email Verification
-router.get('/verify-email/:token', verifyEmail);
-
-// Login
-router.post('/login', loginUser);
-
-// Logout
-router.get('/logout', logoutUser);
-
-router.post('/forgot-password', forgotPassword);
-
-router.post('/reset-password/:token', resetPassword);
-
-
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect('/'); 
-  }
-);
+router.post('/register/volunteer', registerVolunteer); 
+router.post('/login', loginUser); 
 
 module.exports = router;
